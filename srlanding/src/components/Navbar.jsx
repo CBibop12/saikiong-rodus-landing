6961a03539e419246cc8dd72022f4ce0f2f3b7c1
Alex Rekachynskyi <phone.070718@gmail.com>
2025-06-20 12:20:34 +0200

    burger menu updated

--- srlanding/src/components/Navbar.jsx
+++ srlanding/src/components/Navbar.jsx
@@ -1,30 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import LanguageSwitcher from './LanguageSwitcher'
 import { useLang } from '../i18n/LanguageContext'
 import '../styles/Navbar.scss'
+import { Menu as MenuIcon, X as CloseIcon } from 'lucide-react'
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
     const { lang } = useLang()
+
     const toggleMenu = () => setIsOpen(!isOpen)
+
+    // Блокируем скролл страницы когда меню открыто
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'unset'
+        }
+
+        // Очищаем при размонтировании компонента
+        return () => {
+            document.body.style.overflow = 'unset'
+        }
+    }, [isOpen])
 
     return (
         <header className="navbar">
             <nav className="container navbar__container">
-                <button className="navbar__toggle" onClick={toggleMenu}>
-                    <span className="navbar__toggle-bar" />
-                    <span className="navbar__toggle-bar" />
-                    <span className="navbar__toggle-bar" />
-                </button>
+                <Link to="/" className="navbar__logo" onClick={() => setIsOpen(false)}>
+                    <img src={"/assets/images/logoWhite.svg"} alt="logo" />
+                </Link>
+
                 <ul className={`navbar__links navbar__links--left ${isOpen ? 'navbar__links--open' : ''}`}>
                     <li><a href="#about" onClick={() => setIsOpen(false)}>{lang === 'ru' ? 'О нас' : 'About'}</a></li>
                     <li><a href="#heroes" onClick={() => setIsOpen(false)}>{lang === 'ru' ? 'Герои' : 'Heroes'}</a></li>
                 </ul>
-
-                <Link to="/" className="navbar__logo" onClick={() => setIsOpen(false)}>
-                    <img src={"/assets/images/logoWhite.svg"} alt="logo" />
-                </Link>
 
                 <ul className={`navbar__links navbar__links--right ${isOpen ? 'navbar__links--open' : ''}`}>
                     <li><a href="#items" onClick={() => setIsOpen(false)}>{lang === 'ru' ? 'Предметы' : 'Items'}</a></li>
@@ -32,6 +43,10 @@ function Navbar() {
                     <li><a href="#blog" onClick={() => setIsOpen(false)}>{lang === 'ru' ? 'Блог' : 'Blog'}</a></li>
                     <li><LanguageSwitcher /></li>
                 </ul>
+
+                <button className="navbar__toggle" onClick={toggleMenu}>
+                    {isOpen ? <CloseIcon size={32} color="#fff" /> : <MenuIcon size={32} color="#fff" />}
+                </button>
             </nav>
         </header>
     )
